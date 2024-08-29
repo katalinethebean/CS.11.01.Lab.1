92e66a0e2a71712eb80b0bdf621e37f3703b71b1
katherinedai <katherined2027@student.cis.edu.hk>
2024-08-29 16:31:50 +0800

    first commit

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age");
+        int age = scanner.nextInt();
+        if (age > 0) {
+            System.out.println("Your age is " + age);
+            int dateable = age / 2 + 7;
+            System.out.println("a " + age+"-year old should only date somebody who is at least "+dateable+" years old");
+        } else {
+            System.out.println("Please enter valid age");
+        }
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age");
+        int age = scanner.nextInt();
+        if (age>0) {
+            System.out.println("Your age is " + age);
+            int pages = 100 - age;
+            System.out.print(age+"-year olds should read at least "+pages+" pages before giving up a book");
+        }
+        else {
+            System.out.println("Please enter valid age");
+        }
     }
 }
